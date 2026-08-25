885fbba1f1783604993941891076574b3d92e2e6
Sara Manzoor <saramanzoor76@gmail.com>
2026-08-25 15:07:12 +0500

    Add/Update UserProfile page

--- src/pages/UserProfile/UserProfile.jsx
+++ src/pages/UserProfile/UserProfile.jsx
@@ -1,13 +1,30 @@
 import ProfileHero from "../../components/ProfileHero/ProfileHero";
+import RewardAssets from "../../components/RewardAssets/RewardAssets";
+import LevelProgress from "../../components/LevelProgress/LevelProgress";
+import { CurrentAchievement, NextAchievement } from "../../components/AchievementCard/AchievementCard";
 import { userData } from "../../data/userData";
-import { rewardsData } from "../../data/rewardsData";
+import { rewardsData, achievements } from "../../data/rewardsData";
 import styles from "./UserProfile.module.css";
 
 export default function UserProfile() {
+  const handleViewAchievements = () => {
+    // Placeholder until an achievements route exists
+    alert("Achievements page coming soon!");
+  };
+
   return (
     <div className={styles.page}>
       <ProfileHero user={userData} xp={rewardsData.xp} />
-      {/* Next: ProfileIdentity, RewardAssets, LevelProgress, etc. */}
+      <RewardAssets rewards={rewardsData} />
+
+      <div className={styles.twoCol}>
+        <LevelProgress xp={rewardsData.xp} />
+        <div className={styles.achievementStack}>
+          <CurrentAchievement achievement={achievements.current} onViewAll={handleViewAchievements} />
+          <NextAchievement achievement={achievements.next} />
+        </div>
+      </div>
+      {/* Next: WithdrawalOverview */}
     </div>
   );
 }
