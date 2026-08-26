b531e0ef17951be2a32ee6fd2bdad2df1647df5d
Sara Manzoor <saramanzoor76@gmail.com>
2026-08-26 14:19:54 +0500

    chore(hooks): add index.js for centralized exports

---
+++ src/hooks/index.js
@@ -0,0 +1,5 @@
+export { useCountUp } from './useCountUp';
+export { useCopyToClipboard } from './useCopyToClipboard';
+export { useIntersectionObserver } from './useIntersectionObserver';
+export { useWindowSize } from './useWindowSize';
+export { useLocalStorage } from './useLocalStorage';
