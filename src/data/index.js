eacf42ccb36d5c291f137e1d37a249416d4bd2a1
Sara Manzoor <saramanzoor76@gmail.com>
2026-08-26 14:19:39 +0500

    chore(data): add index.js for centralized exports

---
+++ src/data/index.js
@@ -0,0 +1,4 @@
+export { activityData, referralData } from './activityData';
+export { userData } from './userData';
+export { rewardsData, achievements } from './rewardsData';
+export { withdrawalData } from './withdrawalData';
